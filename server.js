7250bec62f964b417dd41e09717ba1254df75c40
raidas <mrraidas@gmail.com>
2024-01-11 11:54:31 +0200

    init IPFS endpoint

--- server.js
+++ server.js
@@ -321,4 +321,32 @@ app.post('/getSecretValueOfIdentity', async (req, res) => {
   })
 })
 
+
+/**
+ * POST /uploadToIPFS
+ * @summary Upload data to IPFS and retrieve CID
+ * @tags IPFS helpers
+ * @param {object} request.body - a json containing data
+ * @example request - payload example
+ * {
+ *   "data": "asdasdasd"
+ * }
+ */
+app.post('/uploadToIPFS', async (req, res) => {
+  const response = await fetch(
+    'https://api.pinata.cloud/pinning/pinJsonToIPFS', {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+      accept: 'application/json', 
+      pinata_api_key: process.env.PINATA_API_KEY,
+      pinata_secret_api_key: process.env.PINATA_API_SECRET,
+    },
+    body: JSON.stringify({ pinataContent: req.body.data })
+  })
+  const response_ = await response.json()
+  return res.send(response_)
+})
+
+
 app.listen(8080, () => console.log(`App's running: http://localhost:8080/`))
